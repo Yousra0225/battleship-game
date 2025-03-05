5951abb329c9aedaca6e356c7847b82e90197c34
Yousra0225 <chbibyousra02@gmail.com>
2025-03-05 15:32:43 +0100

    feat(battleship): add Sea class
    This commit adds the  class, which represents the game board in the battleship game.
    The sea is composed of a grid of  objects, where ships can be placed and attacked.
    
    Features:
    - A constructor to initialize the sea with a given number of rows and columns.

---
+++ src/battleship/Sea.java
@@ -0,0 +1,25 @@
+package battleship;
+package battleship;
+import battleship.util.*;
+/**
+ * Class for Sea
+ */
+
+ public class Sea {
+    private Cell[][] theCells;
+    private int totLifePoints;
+
+    /**
+    * Builds a Sea , the number of rows and colums have given
+    * @param nbRows of this sea
+    * @param nbColoms of this sea
+    */
+    public Sea (int nbRows,int nbColoms){
+        this.theCells=new Cell [nbRows][nbColoms];
+        for (int i=0;i<this.theCells.length;i++){
+            for (int j=0 ; j<this.theCells[i].length;j++){
+                theCells[i][j]=new Cell();
+            }
+        }
+        this.totLifePoints=0;
+    }
