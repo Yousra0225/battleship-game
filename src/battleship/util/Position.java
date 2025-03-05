a2cbcbe78fa153f05cc6806725c751fb46cfe370
Yousra0225 <chbibyousra02@gmail.com>
2025-03-05 15:26:51 +0100

    feat(battleship): add Position class
    This commit adds the  class, which represents a point in a discrete 2D environment.

---
+++ src/battleship/util/Position.java
@@ -0,0 +1,37 @@
+package battleship.util;
+
+/**
+ * a position in a discrete 2D environment, a position is determined by integer
+ * coordinates x and y
+ * 
+ * @author JC
+ *
+ */
+public class Position {
+   private int x;
+   private int y;
+
+   /**
+    * creates a position corresponding to given coordinates
+    * 
+    * @param x the x coordinate
+    * @param y the y coordinate
+    */
+   public Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+   }
+
+   /**
+    * @return the x coordinate
+    */
+   public int getX() {
+      return this.x;
+   }
+
+   /**
+    * @return the y coordinate
+    */
+   public int getY() {
+      return this.y;
+   }
