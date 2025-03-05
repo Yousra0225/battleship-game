fabf4d6c2600cd32732a186a94ba4efc98d9d7d2
Yousra0225 <chbibyousra02@gmail.com>
2025-03-05 15:10:24 +0100

    feat(battleship): add Ship class
    This commit adds the  class to the  package.
    The  class represents a battleship with the following functionalities:
    
    - A constructor to initialize the ship with a given number of life points.
    - A method hasBeenSunk() to check if the ship has been completely destroyed.
    - A method beenHitting() to decrease the ship's life points when hit.
    - A method getLifePoints() to retrieve the current life points.
    - A toString() method to provide a textual representation of the ship.

---
+++ src/battleship/Ship.java
@@ -0,0 +1,44 @@
+package battleship;
+
+/**
+  * Class for Ship
+  */
+  public class Ship {
+    private int lifePoints;
+
+    /** Constructor of Ship class, create a ship and initialize this lifePoints.
+      * @param length number of length/lifePoints
+      */
+    public Ship(int length){
+      this.lifePoints = length;
+    }
+
+    /** Provides the information if this ship has been sunk
+      * @return true if has been sunk, false else
+      */
+    public boolean hasBeenSunk(){
+      return this.getLifePoints() == 0;
+    }
+
+    /**
+      * Remove one point in this lifePoints each time the ship has been hit
+      */
+    public void beenHitting() {
+      this.lifePoints = this.lifePoints - 1;
+    }
+
+    /** Provides the lifePoints of this ship
+      * @return the lifePoints of this ship
+      */
+    public int getLifePoints(){
+      return this.lifePoints;
+    }
+
+    /** Provides a text about this ship
+      * @return a text about this ship
+      */
+    public String toString() {
+      return "Ce bateau a "+ this.getLifePoints() +"points de vie.";
+    }
+
+  }
