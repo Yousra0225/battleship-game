4b4f9b067bf37356fde8e063f43d58bb3dad302c
Yousra0225 <chbibyousra02@gmail.com>
2025-03-09 07:34:06 +0100

    test: Add test for shooting at a ship on the sea

--- test/SeaTest.java
+++ test/SeaTest.java
@@ -6,4 +6,12 @@ import static org.junit.Assert.*;
   * Class for SeaTest
   */
 public class SeaTest {
+    @Test
+    public void shootOnAShipOnSea() {
+    Sea sea = new Sea(5,5);
+    Position p = new Position(1,1);
+    Ship ship = new Ship(2);
+    sea.addShip(ship,p);
+    assertEquals(sea.shoot(p),Answer.HIT);
+  }
 }
