2908dd5cb5b80d87190518ca01b8948fb27817ba
Yousra0225 <chbibyousra02@gmail.com>
2025-03-09 07:27:27 +0100

    test: Add test to verify ship loses one life point when hit
    - Implemented a test to verify that when a ship is hit, its life points decrease by 1.
    - The test checks that the ship's life points are initially correct and decrease after the  method is called.

--- test/ShipTest.java
+++ test/ShipTest.java
@@ -1,3 +1,14 @@
 package battleship; 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+public class ShipTest {
+
+    @Test
+    public void WhereShipHasBeenHittingHeLoseOnePointLife() {
+      Ship ship = new Ship(2);
+      assertTrue(ship.getLifePoints() == 2 );
+      ship.beenHitting();
+      assertTrue(ship.getLifePoints() == 1 );
+    }
+}
