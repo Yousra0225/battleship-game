8ef96e423b68c9b20fafb3449511a720e4fdd016
Yousra0225 <chbibyousra02@gmail.com>
2025-03-10 13:46:23 +0100

    feat(Game): add Game class with constructor

---
+++ src/battleship/Game.java
@@ -0,0 +1,16 @@
+package battleship;
+import battleship.util.*;
+import io.*;
+/**
+  * Class for Game
+  */
+public class Game {
+  private Sea sea;
+
+/** Constructor of the game class, create one game to play
+  * @param sea the sea of game
+  */
+  public Game(Sea sea){
+    this.sea = sea;
+  }
+}
